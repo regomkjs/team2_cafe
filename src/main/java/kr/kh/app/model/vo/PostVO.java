6ce62bf39eb3a0376659b87df933fc836a075eb2
regomkjs <regomkjs2013@gmail.com>
2024-03-06 17:53:14 +0900

    팀과제 구조 추가

---
+++ src/main/java/kr/kh/app/model/vo/PostVO.java
@@ -0,0 +1,12 @@
+package kr.kh.app.model.vo;
+
+public class PostVO {
+ 	private int po_num; 
+ 	private String po_title; 
+ 	private String po_content; 
+ 	private String po_writer;
+ 	private int po_view; 
+ 	private String po_datetime; 
+	private String po_he_num;
+	private String po_me_id;
+}
