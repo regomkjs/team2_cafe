74c0c93c0e4d5d1b5e626ed45643135ca2324e8e
sujinlee12 <153247951+sujinlee12@users.noreply.github.com>
2024-03-07 18:53:22 +0900

    카테고리 구현중

--- src/main/java/kr/kh/app/controller/MainServlet.java
+++ src/main/java/kr/kh/app/controller/MainServlet.java
@@ -1,18 +1,38 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.kh.app.pagination.Criteria;
+import kr.kh.app.service.PostService;
+import kr.kh.app.service.PostServiceImp;
+
 @WebServlet("/")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private PostService postService = new PostServiceImp();
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String search = request.getParameter("search");
+		String type =request.getParameter("type");
+		int page;
+		try {
+			
+			page = Integer.parseInt(request.getParameter("page"));
+		}catch(Exception e) {
+			page = 1;
+		}
+		//검색어, 검색 타입, 현재 페이지, 한 페이지 컨텐츠 개수를 이용하여 현재 페이지 정보 객체를 생성
+		Criteria cri = new Criteria(page, 2, type, search);
+		int totalCount = postService.getTotalCount(cri);
+		ArrayList<PostVO>list = PostService.getPostList(cri);
 		request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
